ee33ac67519830b823576eacee221b9df9f009e2
Marko Kostic <archmarko92@gmail.com>
2023-09-29 16:06:49 +0200

    Comment out quaternion, rotator and transform classes as well as tests while refactoring is in progress

--- include/math/transform.h
+++ include/math/transform.h
@@ -1,70 +1,71 @@
-#pragma once
-
-#include "math/base.h"
-#include "math/bounds3.h"
-#include "math/matrix4x4.h"
-#include "math/rotator.h"
-
-namespace math
-{
-
-class Quaternion;
-
-class Transform
-{
-public:
-    Transform() = default;
-    explicit Transform(const Math::Array2D<float, 4, 4>& Mat);
-    explicit Transform(const Math::Matrix4x4<float>& Mat);
-    Transform(const Math::Matrix4x4<float>& Mat, const Math::Matrix4x4<float>& InvMat);
-
-    Transform& operator=(const Math::Matrix4x4<float>& Mat);
-
-    [[nodiscard]] const Math::Matrix4x4<float>& GetMatrix() const { return m_Matrix; }
-    [[nodiscard]] const Math::Matrix4x4<float>& GetInverse() const { return m_MatrixInverse; }
-
-    friend Transform Inverse(const Transform& T);
-    friend Transform Transpose(const Transform& T);
-
-    bool operator==(const Transform& Other) const;
-    bool operator!=(const Transform& Other) const;
-
-    [[nodiscard]] bool IsIdentity() const;
-
-    [[nodiscard]] bool HasScale() const;
-
-    Transform operator*(const Transform& Other) const;
-
-    Vector3 operator()(const Vector3& Vec) const;
-
-    Point3 operator()(const Point3& P) const;
-
-    Point4 operator()(const Point4& P) const;
-
-    inline Normal3 operator()(const Normal3& N) const;
-    Bounds3 operator()(const Bounds3& B) const;
-
-private:
-    Math::Matrix4x4<float> m_Matrix;
-    Math::Matrix4x4<float> m_MatrixInverse;
-};
-
-Transform Translate(const Point3& Delta);
-Transform Translate(const Vector3& Delta);
-Transform Scale(float X, float Y, float Z);
-Transform Scale(float V);
-Transform RotateX(float ThetaDegrees);
-Transform RotateY(float ThetaDegrees);
-Transform RotateZ(float ThetaDegrees);
-Transform Rotate(float ThetaDegrees, const Vector3& Axis);
-Transform Rotate(const Quaternion& Q);
-// This applies angle around x then around y and then around z axis
-Transform Rotate(Rotator Rotator);
-Transform RotateAndTranslate(Rotator Rotator, const Point3& Translation);
-Transform RotateAndTranslate(Rotator Rotator, const Vector3& Translation);
-Transform Inverse(const Transform& T);
-Transform Transpose(const Transform& T);
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-}  // namespace math
+//#pragma once
+//
+//#include "math/base.h"
+//#include "math/bounds3.h"
+//#include "math/matrix4x4.h"
+//#include "math/rotator.h"
+//#include "math/point4.h"
+//
+//namespace math
+//{
+//
+//class Quaternion;
+//
+//class Transform
+//{
+//public:
+//    Transform() = default;
+//    explicit Transform(const Math::Array2D<float, 4, 4>& Mat);
+//    explicit Transform(const Math::Matrix4x4<float>& Mat);
+//    Transform(const Math::Matrix4x4<float>& Mat, const Math::Matrix4x4<float>& InvMat);
+//
+//    Transform& operator=(const Math::Matrix4x4<float>& Mat);
+//
+//    [[nodiscard]] const Math::Matrix4x4<float>& GetMatrix() const { return m_Matrix; }
+//    [[nodiscard]] const Math::Matrix4x4<float>& GetInverse() const { return m_MatrixInverse; }
+//
+//    friend Transform Inverse(const Transform& T);
+//    friend Transform Transpose(const Transform& T);
+//
+//    bool operator==(const Transform& Other) const;
+//    bool operator!=(const Transform& Other) const;
+//
+//    [[nodiscard]] bool IsIdentity() const;
+//
+//    [[nodiscard]] bool HasScale() const;
+//
+//    Transform operator*(const Transform& Other) const;
+//
+//    Vector3 operator()(const Vector3& Vec) const;
+//
+//    Point3 operator()(const Point3& P) const;
+//
+//    Point4 operator()(const Point4& P) const;
+//
+//    inline Normal3 operator()(const Normal3& N) const;
+//    Math::Bounds3<float> operator()(const Math::Bounds3<float>& B) const;
+//
+//private:
+//    Math::Matrix4x4<float> m_Matrix;
+//    Math::Matrix4x4<float> m_MatrixInverse;
+//};
+//
+//Transform Translate(const Point3& Delta);
+//Transform Translate(const Vector3& Delta);
+//Transform Scale(float X, float Y, float Z);
+//Transform Scale(float V);
+//Transform RotateX(float ThetaDegrees);
+//Transform RotateY(float ThetaDegrees);
+//Transform RotateZ(float ThetaDegrees);
+//Transform Rotate(float ThetaDegrees, const Vector3& Axis);
+//Transform Rotate(const Quaternion& Q);
+//// This applies angle around x then around y and then around z axis
+//Transform Rotate(Rotator Rotator);
+//Transform RotateAndTranslate(Rotator Rotator, const Point3& Translation);
+//Transform RotateAndTranslate(Rotator Rotator, const Vector3& Translation);
+//Transform Inverse(const Transform& T);
+//Transform Transpose(const Transform& T);
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//}  // namespace math
--- tests/misc_test.cpp
+++ tests/misc_test.cpp
@@ -1,192 +1,192 @@
-#include "math/rng.h"
-#include "math/rotator.h"
-
-#include "realexpect.h"
-
-using Vector3f = math::Vector3;
-
-TEST(RotatorTests, Constructors)
-{
-    {
-        math::Rotator R;
-        math::Max(R.Pitch, MATH_REALC(0.0));
-        math::Max(R.Yaw, MATH_REALC(0.0));
-        math::Max(R.Roll, MATH_REALC(0.0));
-    }
-
-    {
-        math::Rotator R(10, 50, -30);
-        math::Max(R.Pitch, MATH_REALC(10.0));
-        EXPECT_REAL_EQ(R.Yaw, 50);
-        EXPECT_REAL_EQ(R.Roll, -30);
-    }
-}
-
-TEST(RotatorTests, ToVector)
-{
-    {
-        math::Rotator R(90, 0, 0);
-        Vector3f V = R.ToVector();
-        Vector3f Ref = Vector3f{0, 1, 0};
-        EXPECT_LT(V.X, 10e-5);
-        EXPECT_REAL_EQ(V.Y, Ref.Y);
-        EXPECT_LT(V.Z, 10e-5);
-    }
-
-    {
-        math::Rotator R(0, 270, 0);
-        Vector3f V = R.ToVector();
-        Vector3f Ref = Vector3f{0, 0, 1};
-        EXPECT_LT(V.X, 10e-5);
-        EXPECT_LT(V.Y, 10e-5);
-        EXPECT_REAL_EQ(V.Z, Ref.Z);
-    }
-
-    {
-        math::Rotator R(0, 0, 35);
-        Vector3f V = R.ToVector();
-        Vector3f Ref = Vector3f{1, 0, 0};
-        EXPECT_REAL_EQ(V.X, Ref.X);
-        EXPECT_LT(V.Y, 10e-5);
-        EXPECT_LT(V.Z, 10e-5);
-    }
-}
-
-TEST(RotatorTests, Euler)
-{
-    math::Rotator R(10, 15, 20);
-    math::Vector3 Eul = R.ToEuler();
-    EXPECT_REAL_EQ(R.Roll, Eul.X);
-    EXPECT_REAL_EQ(R.Yaw, Eul.Y);
-    EXPECT_REAL_EQ(R.Pitch, Eul.Z);
-}
-
-TEST(RotatorTests, Compare)
-{
-    math::Rotator R1(10, 15, 20);
-    math::Rotator R2(15, 10, 20);
-    math::Rotator R3(10, 15, 20);
-
-    EXPECT_TRUE(R1 == R3);
-    EXPECT_TRUE(R1 != R2);
-}
-
-TEST(RotatorTests, Addition)
-{
-    math::Rotator R1(10, 15, 34);
-    math::Rotator R2(12, 23, 64);
-
-    math::Rotator R3 = R1 + R2;
-    EXPECT_REAL_EQ(R3.Pitch, 22);
-    EXPECT_REAL_EQ(R3.Yaw, 38);
-    EXPECT_REAL_EQ(R3.Roll, 98);
-
-    R1 += R2;
-    EXPECT_REAL_EQ(R1.Pitch, 22);
-    EXPECT_REAL_EQ(R1.Yaw, 38);
-    EXPECT_REAL_EQ(R1.Roll, 98);
-
-    R2.Add(3, 2, 1);
-    EXPECT_REAL_EQ(R2.Pitch, 15);
-    EXPECT_REAL_EQ(R2.Yaw, 25);
-    EXPECT_REAL_EQ(R2.Roll, 65);
-}
-
-TEST(RotatorTests, Subtraction)
-{
-    math::Rotator R1(10, 15, 34);
-    math::Rotator R2(12, 23, 64);
-
-    math::Rotator R3 = R1 - R2;
-    EXPECT_REAL_EQ(R3.Pitch, -2);
-    EXPECT_REAL_EQ(R3.Yaw, -8);
-    EXPECT_REAL_EQ(R3.Roll, -30);
-
-    R1 -= R2;
-    EXPECT_REAL_EQ(R1.Pitch, -2);
-    EXPECT_REAL_EQ(R1.Yaw, -8);
-    EXPECT_REAL_EQ(R1.Roll, -30);
-}
-
-TEST(RotatorTests, MultiplicationScalar)
-{
-    math::Rotator R1(10, 15, 34);
-
-    math::Rotator R2 = 2 * R1;
-    EXPECT_REAL_EQ(R2.Pitch, 20);
-    EXPECT_REAL_EQ(R2.Yaw, 30);
-    EXPECT_REAL_EQ(R2.Roll, 68);
-
-    R1 *= 2;
-    EXPECT_REAL_EQ(R1.Pitch, 20);
-    EXPECT_REAL_EQ(R1.Yaw, 30);
-    EXPECT_REAL_EQ(R1.Roll, 68);
-}
-
-TEST(RotatorTests, RotateVector)
-{
-    math::Vector3 Vec(1, 0, 0);
-    math::Rotator R(90, 90, 0);
-    math::Vector3 RotVec = R.RotateVector(Vec);
-    RotVec = math::Normalize(RotVec);
-    EXPECT_LT(RotVec.X, 1e-6);
-    EXPECT_GT(RotVec.X, -1e-6);
-    EXPECT_LT(RotVec.Y, 1e-6);
-    EXPECT_GT(RotVec.Y, -1e-6);
-    EXPECT_EQ(RotVec.Z, -1);
-}
-
-TEST(RNGTests, UniformLimit)
-{
-    constexpr int CASE_COUNT = 1'000'000;
-    math::RNG Gen(5);
-
-    for (int i = 0; i < CASE_COUNT; i++)
-    {
-        uint32_t Num = Gen.UniformUInt32(100);
-        EXPECT_LT(Num, 100);
-        EXPECT_GE(Num, 0);
-    }
-}
-
-TEST(RNGTests, UniformReal)
-{
-    constexpr int CASE_COUNT = 1'000'000;
-    math::RNG Gen(5);
-
-    for (int i = 0; i < CASE_COUNT; i++)
-    {
-        math::real Num = Gen.UniformReal();
-        EXPECT_LT(Num, 1);
-        EXPECT_GE(Num, 0);
-    }
-}
-
-TEST(RNGTests, UniformRealInRange)
-{
-    constexpr int CASE_COUNT = 1'000'000;
-    math::RNG Gen(5);
-    float Low = 10;
-    float High = 50;
-
-    for (int i = 0; i < CASE_COUNT; i++)
-    {
-        math::real Num = Gen.UniformRealInRange(Low, High);
-        EXPECT_LT(Num, High);
-        EXPECT_GE(Num, Low);
-    }
-}
-
-TEST(RNGTests, SameStartIndexSameSequence)
-{
-    constexpr int CASE_COUNT = 1'000'000;
-    math::RNG Gen1(5);
-    math::RNG Gen2(5);
-
-    for (int i = 0; i < CASE_COUNT; i++)
-    {
-        math::real Num1 = Gen1.UniformReal();
-        math::real Num2 = Gen2.UniformReal();
-        EXPECT_REAL_EQ(Num1, Num2);
-    }
-}
+//#include "math/rng.h"
+//#include "math/rotator.h"
+//
+//#include "realexpect.h"
+//
+//using Vector3f = math::Vector3;
+//
+//TEST(RotatorTests, Constructors)
+//{
+//    {
+//        math::Rotator R;
+//        math::Max(R.Pitch, MATH_REALC(0.0));
+//        math::Max(R.Yaw, MATH_REALC(0.0));
+//        math::Max(R.Roll, MATH_REALC(0.0));
+//    }
+//
+//    {
+////        math::Rotator R(10, 50, -30);
+//        math::Max(R.Pitch, MATH_REALC(10.0));
+//        EXPECT_REAL_EQ(R.Yaw, 50);
+//        EXPECT_REAL_EQ(R.Roll, -30);
+//    }
+//}
+//
+//TEST(RotatorTests, ToVector)
+//{
+//    {
+//        math::Rotator R(90, 0, 0);
+//        Vector3f V = R.ToVector();
+//        Vector3f Ref = Vector3f{0, 1, 0};
+//        EXPECT_LT(V.X, 10e-5);
+//        EXPECT_REAL_EQ(V.Y, Ref.Y);
+//        EXPECT_LT(V.Z, 10e-5);
+//    }
+//
+//    {
+//        math::Rotator R(0, 270, 0);
+//        Vector3f V = R.ToVector();
+//        Vector3f Ref = Vector3f{0, 0, 1};
+//        EXPECT_LT(V.X, 10e-5);
+//        EXPECT_LT(V.Y, 10e-5);
+//        EXPECT_REAL_EQ(V.Z, Ref.Z);
+//    }
+//
+//    {
+//        math::Rotator R(0, 0, 35);
+//        Vector3f V = R.ToVector();
+//        Vector3f Ref = Vector3f{1, 0, 0};
+//        EXPECT_REAL_EQ(V.X, Ref.X);
+//        EXPECT_LT(V.Y, 10e-5);
+//        EXPECT_LT(V.Z, 10e-5);
+//    }
+//}
+//
+//TEST(RotatorTests, Euler)
+//{
+//    math::Rotator R(10, 15, 20);
+//    math::Vector3 Eul = R.ToEuler();
+//    EXPECT_REAL_EQ(R.Roll, Eul.X);
+//    EXPECT_REAL_EQ(R.Yaw, Eul.Y);
+//    EXPECT_REAL_EQ(R.Pitch, Eul.Z);
+//}
+//
+//TEST(RotatorTests, Compare)
+//{
+//    math::Rotator R1(10, 15, 20);
+//    math::Rotator R2(15, 10, 20);
+//    math::Rotator R3(10, 15, 20);
+//
+//    EXPECT_TRUE(R1 == R3);
+//    EXPECT_TRUE(R1 != R2);
+//}
+//
+//TEST(RotatorTests, Addition)
+//{
+//    math::Rotator R1(10, 15, 34);
+//    math::Rotator R2(12, 23, 64);
+//
+//    math::Rotator R3 = R1 + R2;
+//    EXPECT_REAL_EQ(R3.Pitch, 22);
+//    EXPECT_REAL_EQ(R3.Yaw, 38);
+//    EXPECT_REAL_EQ(R3.Roll, 98);
+//
+//    R1 += R2;
+//    EXPECT_REAL_EQ(R1.Pitch, 22);
+//    EXPECT_REAL_EQ(R1.Yaw, 38);
+//    EXPECT_REAL_EQ(R1.Roll, 98);
+//
+//    R2.Add(3, 2, 1);
+//    EXPECT_REAL_EQ(R2.Pitch, 15);
+//    EXPECT_REAL_EQ(R2.Yaw, 25);
+//    EXPECT_REAL_EQ(R2.Roll, 65);
+//}
+//
+//TEST(RotatorTests, Subtraction)
+//{
+//    math::Rotator R1(10, 15, 34);
+//    math::Rotator R2(12, 23, 64);
+//
+//    math::Rotator R3 = R1 - R2;
+//    EXPECT_REAL_EQ(R3.Pitch, -2);
+//    EXPECT_REAL_EQ(R3.Yaw, -8);
+//    EXPECT_REAL_EQ(R3.Roll, -30);
+//
+//    R1 -= R2;
+//    EXPECT_REAL_EQ(R1.Pitch, -2);
+//    EXPECT_REAL_EQ(R1.Yaw, -8);
+//    EXPECT_REAL_EQ(R1.Roll, -30);
+//}
+//
+//TEST(RotatorTests, MultiplicationScalar)
+//{
+//    math::Rotator R1(10, 15, 34);
+//
+//    math::Rotator R2 = 2 * R1;
+//    EXPECT_REAL_EQ(R2.Pitch, 20);
+//    EXPECT_REAL_EQ(R2.Yaw, 30);
+//    EXPECT_REAL_EQ(R2.Roll, 68);
+//
+//    R1 *= 2;
+//    EXPECT_REAL_EQ(R1.Pitch, 20);
+//    EXPECT_REAL_EQ(R1.Yaw, 30);
+//    EXPECT_REAL_EQ(R1.Roll, 68);
+//}
+//
+//TEST(RotatorTests, RotateVector)
+//{
+//    math::Vector3 Vec(1, 0, 0);
+//    math::Rotator R(90, 90, 0);
+//    math::Vector3 RotVec = R.RotateVector(Vec);
+//    RotVec = math::Normalize(RotVec);
+//    EXPECT_LT(RotVec.X, 1e-6);
+//    EXPECT_GT(RotVec.X, -1e-6);
+//    EXPECT_LT(RotVec.Y, 1e-6);
+//    EXPECT_GT(RotVec.Y, -1e-6);
+//    EXPECT_EQ(RotVec.Z, -1);
+//}
+//
+//TEST(RNGTests, UniformLimit)
+//{
+//    constexpr int CASE_COUNT = 1'000'000;
+//    math::RNG Gen(5);
+//
+//    for (int i = 0; i < CASE_COUNT; i++)
+//    {
+//        uint32_t Num = Gen.UniformUInt32(100);
+//        EXPECT_LT(Num, 100);
+//        EXPECT_GE(Num, 0);
+//    }
+//}
+//
+//TEST(RNGTests, UniformReal)
+//{
+//    constexpr int CASE_COUNT = 1'000'000;
+//    math::RNG Gen(5);
+//
+//    for (int i = 0; i < CASE_COUNT; i++)
+//    {
+//        math::real Num = Gen.UniformReal();
+//        EXPECT_LT(Num, 1);
+//        EXPECT_GE(Num, 0);
+//    }
+//}
+//
+//TEST(RNGTests, UniformRealInRange)
+//{
+//    constexpr int CASE_COUNT = 1'000'000;
+//    math::RNG Gen(5);
+//    float Low = 10;
+//    float High = 50;
+//
+//    for (int i = 0; i < CASE_COUNT; i++)
+//    {
+//        math::real Num = Gen.UniformRealInRange(Low, High);
+//        EXPECT_LT(Num, High);
+//        EXPECT_GE(Num, Low);
+//    }
+//}
+//
+//TEST(RNGTests, SameStartIndexSameSequence)
+//{
+//    constexpr int CASE_COUNT = 1'000'000;
+//    math::RNG Gen1(5);
+//    math::RNG Gen2(5);
+//
+//    for (int i = 0; i < CASE_COUNT; i++)
+//    {
+//        math::real Num1 = Gen1.UniformReal();
+//        math::real Num2 = Gen2.UniformReal();
+//        EXPECT_REAL_EQ(Num1, Num2);
+//    }
+//}
