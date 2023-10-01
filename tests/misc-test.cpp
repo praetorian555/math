0129ee286e037f154e79367cd1c8804ad18cd56a
Marko Kostic <archmarko92@gmail.com>
2023-10-01 13:59:56 +0200

    Refactor Rotator class to be a template

--- tests/misc-test.cpp
+++ tests/misc-test.cpp
@@ -1,141 +1,214 @@
 #include <gtest/gtest.h>
 
 #include "math/rng.h"
-//#include "math/rotator.h"
-//
-//
-//using Vector3f = Math::Vector3;
-//
-//TEST(RotatorTests, Constructors)
-//{
-//    {
-//        Math::Rotator R;
-//        Math::Max(R.Pitch, MATH_REALC(0.0));
-//        Math::Max(R.Yaw, MATH_REALC(0.0));
-//        Math::Max(R.Roll, MATH_REALC(0.0));
-//    }
-//
-//    {
-////        Math::Rotator R(10, 50, -30);
-//        Math::Max(R.Pitch, MATH_REALC(10.0));
-//        EXPECT_REAL_EQ(R.Yaw, 50);
-//        EXPECT_REAL_EQ(R.Roll, -30);
-//    }
-//}
-//
-//TEST(RotatorTests, ToVector)
-//{
-//    {
-//        Math::Rotator R(90, 0, 0);
-//        Vector3f V = R.ToVector();
-//        Vector3f Ref = Vector3f{0, 1, 0};
-//        EXPECT_LT(V.X, 10e-5);
-//        EXPECT_REAL_EQ(V.Y, Ref.Y);
-//        EXPECT_LT(V.Z, 10e-5);
-//    }
-//
-//    {
-//        Math::Rotator R(0, 270, 0);
-//        Vector3f V = R.ToVector();
-//        Vector3f Ref = Vector3f{0, 0, 1};
-//        EXPECT_LT(V.X, 10e-5);
-//        EXPECT_LT(V.Y, 10e-5);
-//        EXPECT_REAL_EQ(V.Z, Ref.Z);
-//    }
-//
-//    {
-//        Math::Rotator R(0, 0, 35);
-//        Vector3f V = R.ToVector();
-//        Vector3f Ref = Vector3f{1, 0, 0};
-//        EXPECT_REAL_EQ(V.X, Ref.X);
-//        EXPECT_LT(V.Y, 10e-5);
-//        EXPECT_LT(V.Z, 10e-5);
-//    }
-//}
-//
-//TEST(RotatorTests, Euler)
-//{
-//    Math::Rotator R(10, 15, 20);
-//    Math::Vector3 Eul = R.ToEuler();
-//    EXPECT_REAL_EQ(R.Roll, Eul.X);
-//    EXPECT_REAL_EQ(R.Yaw, Eul.Y);
-//    EXPECT_REAL_EQ(R.Pitch, Eul.Z);
-//}
-//
-//TEST(RotatorTests, Compare)
-//{
-//    Math::Rotator R1(10, 15, 20);
-//    Math::Rotator R2(15, 10, 20);
-//    Math::Rotator R3(10, 15, 20);
-//
-//    EXPECT_TRUE(R1 == R3);
-//    EXPECT_TRUE(R1 != R2);
-//}
-//
-//TEST(RotatorTests, Addition)
-//{
-//    Math::Rotator R1(10, 15, 34);
-//    Math::Rotator R2(12, 23, 64);
-//
-//    Math::Rotator R3 = R1 + R2;
-//    EXPECT_REAL_EQ(R3.Pitch, 22);
-//    EXPECT_REAL_EQ(R3.Yaw, 38);
-//    EXPECT_REAL_EQ(R3.Roll, 98);
-//
-//    R1 += R2;
-//    EXPECT_REAL_EQ(R1.Pitch, 22);
-//    EXPECT_REAL_EQ(R1.Yaw, 38);
-//    EXPECT_REAL_EQ(R1.Roll, 98);
-//
-//    R2.Add(3, 2, 1);
-//    EXPECT_REAL_EQ(R2.Pitch, 15);
-//    EXPECT_REAL_EQ(R2.Yaw, 25);
-//    EXPECT_REAL_EQ(R2.Roll, 65);
-//}
-//
-//TEST(RotatorTests, Subtraction)
-//{
-//    Math::Rotator R1(10, 15, 34);
-//    Math::Rotator R2(12, 23, 64);
-//
-//    Math::Rotator R3 = R1 - R2;
-//    EXPECT_REAL_EQ(R3.Pitch, -2);
-//    EXPECT_REAL_EQ(R3.Yaw, -8);
-//    EXPECT_REAL_EQ(R3.Roll, -30);
-//
-//    R1 -= R2;
-//    EXPECT_REAL_EQ(R1.Pitch, -2);
-//    EXPECT_REAL_EQ(R1.Yaw, -8);
-//    EXPECT_REAL_EQ(R1.Roll, -30);
-//}
-//
-//TEST(RotatorTests, MultiplicationScalar)
-//{
-//    Math::Rotator R1(10, 15, 34);
-//
-//    Math::Rotator R2 = 2 * R1;
-//    EXPECT_REAL_EQ(R2.Pitch, 20);
-//    EXPECT_REAL_EQ(R2.Yaw, 30);
-//    EXPECT_REAL_EQ(R2.Roll, 68);
-//
-//    R1 *= 2;
-//    EXPECT_REAL_EQ(R1.Pitch, 20);
-//    EXPECT_REAL_EQ(R1.Yaw, 30);
-//    EXPECT_REAL_EQ(R1.Roll, 68);
-//}
-//
-//TEST(RotatorTests, RotateVector)
-//{
-//    Math::Vector3 Vec(1, 0, 0);
-//    Math::Rotator R(90, 90, 0);
-//    Math::Vector3 RotVec = R.RotateVector(Vec);
-//    RotVec = Math::Normalize(RotVec);
-//    EXPECT_LT(RotVec.X, 1e-6);
-//    EXPECT_GT(RotVec.X, -1e-6);
-//    EXPECT_LT(RotVec.Y, 1e-6);
-//    EXPECT_GT(RotVec.Y, -1e-6);
-//    EXPECT_EQ(RotVec.Z, -1);
-//}
+#include "math/rotator.h"
+
+using Rotf = Math::Rotator<float>;
+using Vector3f = Math::Vector3<float>;
+using Rotd = Math::Rotator<double>;
+using Vector3d = Math::Vector3<double>;
+
+TEST(RotatorTests, Constructors)
+{
+    {
+        const Rotf rot1 = Rotf::Zero();
+        EXPECT_FLOAT_EQ(rot1.pitch, 0.0f);
+        EXPECT_FLOAT_EQ(rot1.yaw, 0.0f);
+        EXPECT_FLOAT_EQ(rot1.roll, 0.0f);
+        const Rotf rot2(10, 50, -30);
+        EXPECT_FLOAT_EQ(rot2.pitch, 10.0f);
+        EXPECT_FLOAT_EQ(rot2.yaw, 50.0f);
+        EXPECT_FLOAT_EQ(rot2.roll, -30.0f);
+    }
+    {
+        const Rotd rot1 = Rotd::Zero();
+        EXPECT_DOUBLE_EQ(rot1.pitch, 0.0);
+        EXPECT_DOUBLE_EQ(rot1.yaw, 0.0);
+        EXPECT_DOUBLE_EQ(rot1.roll, 0.0);
+        const Rotd rot2(10, 50, -30);
+        EXPECT_DOUBLE_EQ(rot2.pitch, 10.0);
+        EXPECT_DOUBLE_EQ(rot2.yaw, 50.0);
+        EXPECT_DOUBLE_EQ(rot2.roll, -30.0);
+    }
+}
+
+TEST(RotatorTests, Compare)
+{
+    {
+        const Rotf rot1(10, 15, 20);
+        const Rotf rot2(15, 10, 20);
+        const Rotf rot3(10, 15, 20);
+        EXPECT_TRUE(rot1 == rot3);
+        EXPECT_TRUE(rot1 != rot2);
+    }
+    {
+        const Rotd rot1(10, 15, 20);
+        const Rotd rot2(15, 10, 20);
+        const Rotd rot3(10, 15, 20);
+        EXPECT_TRUE(rot1 == rot3);
+        EXPECT_TRUE(rot1 != rot2);
+    }
+}
+
+TEST(RotatorTests, Addition)
+{
+    {
+        Rotf rot1(10, 15, 34);
+        const Rotf rot2(12, 23, 64);
+
+        const Rotf rot3 = rot1 + rot2;
+        EXPECT_FLOAT_EQ(rot3.pitch, 22);
+        EXPECT_FLOAT_EQ(rot3.yaw, 38);
+        EXPECT_FLOAT_EQ(rot3.roll, 98);
+
+        rot1 += rot2;
+        EXPECT_FLOAT_EQ(rot1.pitch, 22);
+        EXPECT_FLOAT_EQ(rot1.yaw, 38);
+        EXPECT_FLOAT_EQ(rot1.roll, 98);
+    }
+    {
+        Rotd rot1(10, 15, 34);
+        const Rotd rot2(12, 23, 64);
+
+        const Rotd rot3 = rot1 + rot2;
+        EXPECT_DOUBLE_EQ(rot3.pitch, 22);
+        EXPECT_DOUBLE_EQ(rot3.yaw, 38);
+        EXPECT_DOUBLE_EQ(rot3.roll, 98);
+
+        rot1 += rot2;
+        EXPECT_DOUBLE_EQ(rot1.pitch, 22);
+        EXPECT_DOUBLE_EQ(rot1.yaw, 38);
+        EXPECT_DOUBLE_EQ(rot1.roll, 98);
+    }
+}
+
+TEST(RotatorTests, Subtraction)
+{
+    {
+        Rotf rot1(10, 15, 34);
+        const Rotf rot2(12, 23, 64);
+
+        const Rotf rot3 = rot1 - rot2;
+        EXPECT_FLOAT_EQ(rot3.pitch, -2);
+        EXPECT_FLOAT_EQ(rot3.yaw, -8);
+        EXPECT_FLOAT_EQ(rot3.roll, -30);
+
+        rot1 -= rot2;
+        EXPECT_FLOAT_EQ(rot1.pitch, -2);
+        EXPECT_FLOAT_EQ(rot1.yaw, -8);
+        EXPECT_FLOAT_EQ(rot1.roll, -30);
+    }
+    {
+        Rotd rot1(10, 15, 34);
+        const Rotd rot2(12, 23, 64);
+
+        const Rotd rot3 = rot1 - rot2;
+        EXPECT_DOUBLE_EQ(rot3.pitch, -2);
+        EXPECT_DOUBLE_EQ(rot3.yaw, -8);
+        EXPECT_DOUBLE_EQ(rot3.roll, -30);
+
+        rot1 -= rot2;
+        EXPECT_DOUBLE_EQ(rot1.pitch, -2);
+        EXPECT_DOUBLE_EQ(rot1.yaw, -8);
+        EXPECT_DOUBLE_EQ(rot1.roll, -30);
+    }
+}
+
+TEST(RotatorTests, MultiplicationScalar)
+{
+    {
+        Rotf rot1(10, 15, 34);
+
+        const Rotf rot2 = 2.0f * rot1;
+        EXPECT_FLOAT_EQ(rot2.pitch, 20);
+        EXPECT_FLOAT_EQ(rot2.yaw, 30);
+        EXPECT_FLOAT_EQ(rot2.roll, 68);
+
+        rot1 *= 2;
+        EXPECT_FLOAT_EQ(rot1.pitch, 20);
+        EXPECT_FLOAT_EQ(rot1.yaw, 30);
+        EXPECT_FLOAT_EQ(rot1.roll, 68);
+    }
+    {
+        Rotd rot1(10, 15, 34);
+
+        const Rotd rot2 = 2.0 * rot1;
+        EXPECT_DOUBLE_EQ(rot2.pitch, 20);
+        EXPECT_DOUBLE_EQ(rot2.yaw, 30);
+        EXPECT_DOUBLE_EQ(rot2.roll, 68);
+
+        rot1 *= 2;
+        EXPECT_DOUBLE_EQ(rot1.pitch, 20);
+        EXPECT_DOUBLE_EQ(rot1.yaw, 30);
+        EXPECT_DOUBLE_EQ(rot1.roll, 68);
+    }
+}
+
+TEST(RotatorTests, ToVector)
+{
+    {
+        const Rotf rot1(90, 0, 0);
+        const Vector3f vec1 = Math::RotatorToVector(rot1);
+        const Vector3f ref1 = Vector3f{0, 1, 0};
+        EXPECT_TRUE(Math::IsEqual(vec1.x, ref1.x, 0.00001f));
+        EXPECT_TRUE(Math::IsEqual(vec1.y, ref1.y, 0.00001f));
+        EXPECT_TRUE(Math::IsEqual(vec1.z, ref1.z, 0.00001f));
+
+        const Rotf rot2(0, 270, 0);
+        const Vector3f vec2 = Math::RotatorToVector(rot2);
+        const Vector3f ref2 = Vector3f{0, 0, 1};
+        EXPECT_TRUE(Math::IsEqual(vec2.x, ref2.x, 0.00001f));
+        EXPECT_TRUE(Math::IsEqual(vec2.y, ref2.y, 0.00001f));
+        EXPECT_TRUE(Math::IsEqual(vec2.z, ref2.z, 0.00001f));
+
+        const Rotf rot3(0, 0, 35);
+        const Vector3f vec3 = Math::RotatorToVector(rot3);
+        const Vector3f ref3 = Vector3f{1, 0, 0};
+        EXPECT_TRUE(Math::IsEqual(vec3.x, ref3.x, 0.00001f));
+        EXPECT_TRUE(Math::IsEqual(vec3.y, ref3.y, 0.00001f));
+        EXPECT_TRUE(Math::IsEqual(vec3.z, ref3.z, 0.00001f));
+    }
+    {
+        const Rotd rot1(90, 0, 0);
+        const Vector3d vec1 = Math::RotatorToVector(rot1);
+        const Vector3d ref1 = Vector3d{0, 1, 0};
+        EXPECT_TRUE(Math::IsEqual(vec1.x, ref1.x, 0.00001));
+        EXPECT_TRUE(Math::IsEqual(vec1.y, ref1.y, 0.00001));
+        EXPECT_TRUE(Math::IsEqual(vec1.z, ref1.z, 0.00001));
+
+        const Rotd rot2(0, 270, 0);
+        const Vector3d vec2 = Math::RotatorToVector(rot2);
+        const Vector3d ref2 = Vector3d{0, 0, 1};
+        EXPECT_TRUE(Math::IsEqual(vec2.x, ref2.x, 0.00001));
+        EXPECT_TRUE(Math::IsEqual(vec2.y, ref2.y, 0.00001));
+        EXPECT_TRUE(Math::IsEqual(vec2.z, ref2.z, 0.00001));
+
+        const Rotd rot3(0, 0, 35);
+        const Vector3d vec3 = Math::RotatorToVector(rot3);
+        const Vector3d ref3 = Vector3d{1, 0, 0};
+        EXPECT_TRUE(Math::IsEqual(vec3.x, ref3.x, 0.00001));
+        EXPECT_TRUE(Math::IsEqual(vec3.y, ref3.y, 0.00001));
+        EXPECT_TRUE(Math::IsEqual(vec3.z, ref3.z, 0.00001));
+    }
+}
+
+TEST(RotatorTests, Euler)
+{
+    {
+        const Rotf rot(10, 15, 20);
+        const Vector3f euler = Math::RotatorToEuler(rot);
+        EXPECT_FLOAT_EQ(rot.roll, euler.x);
+        EXPECT_FLOAT_EQ(rot.yaw, euler.y);
+        EXPECT_FLOAT_EQ(rot.pitch, euler.z);
+    }
+    {
+        const Rotd rot(10, 15, 20);
+        Vector3d euler = Math::RotatorToEuler(rot);
+        EXPECT_DOUBLE_EQ(rot.roll, euler.x);
+        EXPECT_DOUBLE_EQ(rot.yaw, euler.y);
+        EXPECT_DOUBLE_EQ(rot.pitch, euler.z);
+    }
+}
 
 TEST(RNGTests, UniformLimit)
 {
